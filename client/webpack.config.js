29616b4283c064c51c368e460079f877aeb25970
Ilves Alexandr <Sandrorus76@gmail.com>
2024-12-08 02:11:39 -0600

    1

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,28 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.js', // Точка входа
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'), // Папка для сборки
+        filename: 'bundle.js', // Имя результирующего файла
     },
     mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/, // Файлы .js и .jsx
+                exclude: /node_modules/, // Исключить node_modules
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'], // Разрешаем файлы .js и .jsx
+    },
 };
