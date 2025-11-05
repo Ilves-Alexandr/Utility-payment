b261bce2be8af88457d440290ca99137aa602731
Ilves Alexandr <151178705+Ilves-Alexandr@users.noreply.github.com>
2025-11-05 07:52:51 -0600

    Update tgk.js

--- routes/tgk.js
+++ routes/tgk.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const Tgk = require('../models/TGK');
 const User = require('../models/User')
+const checkRole = require('../middleware/role');
 
 dotenv.config();
 
@@ -24,7 +25,7 @@ function auth(req, res, next) {
 }
 
 // Добавить транзакцию и вычислить сальдо и долг
-router.post('/tgk', auth, async (req, res) => {
+router.post('/tgk', auth, checkRole(['editor', 'admin']), async (req, res) => {
     try {
         // Преобразуем входные данные в числа
         const amountBilled = Number(req.body.amountBilled);
@@ -85,7 +86,7 @@ router.post('/tgk', auth, async (req, res) => {
     }
 });
 
-router.get('/tgks', auth, async (req, res) => {
+router.get('/tgks', auth, checkRole(['viewer', 'editor', 'admin']), async (req, res) => {
     console.log('Запрос на /api/tgks получен')
     try {
         // Получаем общую сумму начисленных средств
@@ -178,7 +179,7 @@ router.get('/tgks', auth, async (req, res) => {
 
 
 // Маршрут для удаления всех транзакций
-router.delete('/tgks', auth, async (req, res) => {
+router.delete('/tgks', auth, checkRole(['admin']), async (req, res) => {
     try {
         const result = await Tgk.deleteMany({});
         res.json({ msg: `Удалено документов: ${result.deletedCount}` });
@@ -189,7 +190,7 @@ router.delete('/tgks', auth, async (req, res) => {
 });
 
 // Удаление транзакции по ID
-router.delete('/tgk/:id', auth, async (req, res) => {
+router.delete('/tgk/:id', auth, checkRole(['editor', 'admin']), async (req, res) => {
     const { id } = req.params;
     try {
         const transaction = await Tgk.findById(id);
@@ -204,7 +205,7 @@ router.delete('/tgk/:id', auth, async (req, res) => {
     }
 });
 
-router.get('/tgk/:id', auth, async (req, res) => {
+router.get('/tgk/:id', auth, checkRole(['viewer', 'editor', 'admin']), async (req, res) => {
     const transactionId = req.params.id;
     try {
         const transaction = await Tgk.findById(transactionId);
@@ -218,7 +219,7 @@ router.get('/tgk/:id', auth, async (req, res) => {
 });
 
 //  Редактирование транзакции по ID
-router.put('/tgk/:id', auth, async (req, res) => {
+router.put('/tgk/:id', auth, checkRole(['editor', 'admin']), async (req, res) => {
     const { id } = req.params;
     const { amountBilled, amountPaid } = req.body;
     // Проверка, что amountBilled и amountPaid — это числа
